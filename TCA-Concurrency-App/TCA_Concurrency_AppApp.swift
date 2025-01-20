55f5e0c38f0e355ee75ec1ce08b9a8986b99c71f
SK3620 <k.n.t11193620@gmail.com>
2025-01-20 17:06:27 +0900

    Add: Initial Commit

---
+++ TCA-Concurrency-App/TCA_Concurrency_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Concurrency_AppApp.swift
+//  TCA-Concurrency-App
+//
+//  Created by 鈴木 健太 on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Concurrency_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
