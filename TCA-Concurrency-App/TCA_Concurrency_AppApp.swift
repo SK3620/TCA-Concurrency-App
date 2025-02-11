738022250e4d0d3298b28e6ecb51815632d22c37
SK3620 <k.n.t11193620@gmail.com>
2025-02-11 22:48:33 +0900

    Update: エントリーポイントを定義

--- TCA-Concurrency-App/TCA_Concurrency_AppApp.swift
+++ TCA-Concurrency-App/TCA_Concurrency_AppApp.swift
@@ -5,13 +5,20 @@
 //  Created by 鈴木 健太 on 2025/01/20.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
-struct TCA_Concurrency_AppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct MyApp: App {
+  static let store = Store(initialState: CounterFeature.State()) {
+    CounterFeature()
+      ._printChanges()
+      // printChanges: Reducer が処理するすべてのアクションがコンソールに出力され、アクションの処理後に状態がどのように変化したかが出力されます
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      CounterView(store: MyApp.store)
     }
+  }
 }
